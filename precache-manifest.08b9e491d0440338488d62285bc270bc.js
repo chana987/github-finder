6becb923c17b8abd73fb79c330366aa56e97a9c6
chana987 <chana987@gmail.com>
2020-02-13 13:48:04 +0200

    Updates

---
+++ precache-manifest.08b9e491d0440338488d62285bc270bc.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b95d604d79fd7609ac6b221000b6bc6",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "c606b9af125e38940730",
+    "url": "/github-finder/static/css/2.03388162.chunk.css"
+  },
+  {
+    "revision": "36797f1943dfe04d885f",
+    "url": "/github-finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "c606b9af125e38940730",
+    "url": "/github-finder/static/js/2.d6bc20a0.chunk.js"
+  },
+  {
+    "revision": "41dec1805a1138a227a108b795866cf1",
+    "url": "/github-finder/static/js/2.d6bc20a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36797f1943dfe04d885f",
+    "url": "/github-finder/static/js/main.05a1014d.chunk.js"
+  },
+  {
+    "revision": "000d33c1b4ecbc4c735b",
+    "url": "/github-finder/static/js/runtime-main.93b7697d.js"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/github-finder/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/github-finder/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/github-finder/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/github-finder/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/github-finder/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/github-finder/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/github-finder/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/github-finder/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/github-finder/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/github-finder/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/github-finder/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/github-finder/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/github-finder/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/github-finder/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/github-finder/static/media/fa-solid-900.ef3df984.eot"
+  }
+]);
